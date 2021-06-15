d32ffa98c8528f90114f81b152fccab8502c9c8a
LucianoMariotti <luciano.mariotti99@gmail.com>
2021-06-15 14:52:56 -0300

    bugs en el nav

--- js/scripts.js
+++ js/scripts.js
@@ -50,4 +50,10 @@ window.addEventListener('DOMContentLoaded', event => {
             }
         });
     });
+
+    // Activate SimpleLightbox plugin for portfolio items
+    new SimpleLightbox({
+        elements: '#portfolio a.portfolio-box'
+    });
+
 });
