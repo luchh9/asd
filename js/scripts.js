016bc8ab1398c0886419becf7e17efacd1b31f71
LucianoMariotti <luciano.mariotti99@gmail.com>
2021-09-30 00:06:45 -0300

    actualizacion de tecnogologias

--- js/scripts.js
+++ js/scripts.js
@@ -1,86 +1,86 @@
 /*!
-* Start Bootstrap - Creative v7.0.2 (https://startbootstrap.com/theme/creative)
-* Copyright 2013-2021 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-creative/blob/master/LICENSE)
-*/
+ * Start Bootstrap - Creative v7.0.2 (https://startbootstrap.com/theme/creative)
+ * Copyright 2013-2021 Start Bootstrap
+ * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-creative/blob/master/LICENSE)
+ */
 //
 // Scripts
-// 
+//
 
-window.addEventListener('DOMContentLoaded', event => {
-
-    // Navbar shrink function
-    var navbarShrink = function () {
-        const navbarCollapsible = document.body.querySelector('#mainNav');
-        if (!navbarCollapsible) {
-            return;
-        }
-        if (window.scrollY === 0) {
-            navbarCollapsible.classList.remove('navbar-shrink')
-        } else {
-            navbarCollapsible.classList.add('navbar-shrink')
-        }
-
-    };
-
-    // Shrink the navbar 
-    navbarShrink();
-
-    // Shrink the navbar when page is scrolled
-    document.addEventListener('scroll', navbarShrink);
-
-    // Activate Bootstrap scrollspy on the main nav element
-    const mainNav = document.body.querySelector('#mainNav');
-    if (mainNav) {
-        new bootstrap.ScrollSpy(document.body, {
-            target: '#mainNav',
-            offset: 74,
-        });
-    };
-
-    // Collapse responsive navbar when toggler is visible
-    const navbarToggler = document.body.querySelector('.navbar-toggler');
-    const responsiveNavItems = [].slice.call(
-        document.querySelectorAll('#navbarResponsive .nav-link')
-    );
-    responsiveNavItems.map(function (responsiveNavItem) {
-        responsiveNavItem.addEventListener('click', () => {
-            if (window.getComputedStyle(navbarToggler).display !== 'none') {
-                navbarToggler.click();
-            }
-        });
-    });
-
-    // Activate SimpleLightbox plugin for portfolio items
-    new SimpleLightbox({
-        elements: '#portfolio a.portfolio-box'
-    });
-
-    function animacioninicial (){      
-        gsap.from('.sub', {
-            duration: .7,
-            y: -200,
-            scale: 1,  
-        })
-        gsap.from('.titulo', {
-            duration: 8,
-            x: -100,
-            opacity: 0,
-            ease: 'elastic'
-        })
-
-        gsap.fromTo('#logo1',
-            {x:-200},
-            {duration: 6,x:4, y:-35,ease:'elastic'}
-            )
-
-        gsap.fromTo('#logo2',
-        {x: 300},
-        {duration: 6,x:-50,ease:'elastic'}
-        )
-
-
+window.addEventListener("DOMContentLoaded", (event) => {
+  // Navbar shrink function
+  var navbarShrink = function () {
+    const navbarCollapsible = document.body.querySelector("#mainNav");
+    if (!navbarCollapsible) {
+      return;
     }
+    if (window.scrollY === 0) {
+      navbarCollapsible.classList.remove("navbar-shrink");
+    } else {
+      navbarCollapsible.classList.add("navbar-shrink");
+    }
+  };
 
-    animacioninicial();
+  // Shrink the navbar
+  navbarShrink();
+
+  // Shrink the navbar when page is scrolled
+  document.addEventListener("scroll", navbarShrink);
+
+  // Activate Bootstrap scrollspy on the main nav element
+  const mainNav = document.body.querySelector("#mainNav");
+  if (mainNav) {
+    new bootstrap.ScrollSpy(document.body, {
+      target: "#mainNav",
+      offset: 74,
+    });
+  }
+
+  // Collapse responsive navbar when toggler is visible
+  const navbarToggler = document.body.querySelector(".navbar-toggler");
+  const responsiveNavItems = [].slice.call(
+    document.querySelectorAll("#navbarResponsive .nav-link")
+  );
+  responsiveNavItems.map(function (responsiveNavItem) {
+    responsiveNavItem.addEventListener("click", () => {
+      if (window.getComputedStyle(navbarToggler).display !== "none") {
+        navbarToggler.click();
+      }
+    });
+  });
+
+  // Activate SimpleLightbox plugin for portfolio items
+  new SimpleLightbox({
+    elements: "#portfolio a.portfolio-box",
+  });
+
+  function animacioninicial() {
+    gsap.from(".sub", {
+      duration: 0.7,
+      y: -200,
+      scale: 1,
+    });
+    gsap.from(".titulo", {
+      duration: 8,
+      x: -100,
+      opacity: 0,
+      ease: "elastic",
+    });
+
+    gsap.fromTo(
+      "#logo1",
+      { x: -200 },
+      { duration: 6, x: 4, y: -15, ease: "elastic" }
+    );
+
+    gsap.fromTo(
+      "#logo2",
+
+      { x: 300 },
+
+      { duration: 6, x: -50, ease: "elastic" }
+    );
+  }
+
+  animacioninicial();
 });
